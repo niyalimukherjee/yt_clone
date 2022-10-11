952b884cf9312206e4ae9552eadfde66f0629f99
niyalimukherjee <seci24niyali@gmail.com>
2022-10-11 20:27:49 +0530

    Search_fn1

--- src/App.js
+++ src/App.js
@@ -2,20 +2,37 @@ import Header from './Header';
 import './App.css';
 import Sidebar from './Sidebar';
 import RecommendedVideos from './RecommendedVideos';
+import {BrowserRouter as Router,Route, Switch} from "react-router-dom"
 function App() {
   return (
     //BEM class name convention
     <div className="app">
+      <Router>
+      <Header/>
+        <Switch>
+         
+          <Route path='/search/:searchTerm'>
+            
+            <h1>Search Page</h1>
+          </Route>
+         
+         
       {/* <h1>Hello ,yt clone </h1> */}
       {/* Header--><Header/> */}
-      <Header/>
+      
+      <Route path='/'>
+      
      
       <div className='app__page'>
-      {/* Sidebar */}
-      <Sidebar />
+      
+      <Sidebar /> 
       {/* Recommended Videos */}
       <RecommendedVideos />
-      </div>
+     
+      </div> 
+      </Route>
+      </Switch>
+      </Router>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React,{useState} from 'react'
 import MenuSharpIcon from '@material-ui/icons/MenuSharp';
 import SearchIcon from '@material-ui/icons/Search';
 import VideoCallIcon from '@material-ui/icons/VideoCall';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import { Avatar } from '@material-ui/core';
+import {Link} from "react-router-dom"
 import './Header.css';
 function Header() {
+  const [inputSearch,setInputSearch]=useState('');
   return (
     <div className='header'>
         {/* <h1>Header</h1> */}
         <div className='header__left'>
         <MenuSharpIcon />
+        <Link to='/'>
         <img className="header__logo"src='https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg' alt=''/>
+        </Link>
         </div>
         <div className='header__input'>
-        <input placeholder='Search' type="text" />
+        <input onChange={(e)=> setInputSearch(e.target.value)} value={inputSearch} placeholder='Search' type="text" />
+        <Link to= {`/search/${inputSearch}`}>
         <SearchIcon className='header__inputButton'/>
+      
+       </Link>
         </div>
         <div className='header__icons'>
         <VideoCallIcon className='header__icon'/>
