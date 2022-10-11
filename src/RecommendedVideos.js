63f6f7e6ce04598849cad782a9b2cf92ef41b655
niyalimukherjee <seci24niyali@gmail.com>
2022-10-11 17:50:58 +0530

    VideoCard_final

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -22,6 +22,27 @@ function RecommendedVideos() {
           channelImage='https://i.picsum.photos/id/1014/6016/4000.jpg?hmac=yMXsznFliL_Y2E2M-qZEsOZE1micNu8TwgNlHj7kzs8'
           channel='Christian'
           image='https://i.picsum.photos/id/1000/5626/3635.jpg?hmac=qWh065Fr_M8Oa3sNsdDL8ngWXv2Jb-EE49ZIn6c0P-g'/>
+          <VideoCard 
+          title='Whether we like it or not..'
+          views='2M views'
+          timestamp='4 days ago'
+          channelImage='https://i.picsum.photos/id/1014/6016/4000.jpg?hmac=yMXsznFliL_Y2E2M-qZEsOZE1micNu8TwgNlHj7kzs8'
+          channel='Christian'
+          image='https://i.picsum.photos/id/1000/5626/3635.jpg?hmac=qWh065Fr_M8Oa3sNsdDL8ngWXv2Jb-EE49ZIn6c0P-g'/>
+          <VideoCard 
+          title='Whether we like it or not..'
+          views='2M views'
+          timestamp='4 days ago'
+          channelImage='https://i.picsum.photos/id/1014/6016/4000.jpg?hmac=yMXsznFliL_Y2E2M-qZEsOZE1micNu8TwgNlHj7kzs8'
+          channel='Christian'
+          image='https://i.picsum.photos/id/1000/5626/3635.jpg?hmac=qWh065Fr_M8Oa3sNsdDL8ngWXv2Jb-EE49ZIn6c0P-g'/>
+          <VideoCard 
+          title='Whether we like it or not..'
+          views='2M views'
+          timestamp='4 days ago'
+          channelImage='https://i.picsum.photos/id/1014/6016/4000.jpg?hmac=yMXsznFliL_Y2E2M-qZEsOZE1micNu8TwgNlHj7kzs8'
+          channel='Christian'
+          image='https://i.picsum.photos/id/1000/5626/3635.jpg?hmac=qWh065Fr_M8Oa3sNsdDL8ngWXv2Jb-EE49ZIn6c0P-g'/>
         </div>
         </div>
   )
--- src/VideoCard.js
+++ src/VideoCard.js
@@ -5,10 +5,10 @@ function VideoCard({image,title,channel,views,timestamp,channelImage}) {
     <div className='videoCard'>
        {/* <h4> VideoCard</h4> */}
        <div>
-        <img src={image} alt=''/>
+        <img className='videoCard__thumbnail' src={image} alt=''/>
         <div className='videoCard__info'>
             <Avatar className='videoCard__avatar' alt={channel} src={channelImage}/>
-            <div className='video__text'>
+            <div className='videoCard__text'>
                 <h4>{title}</h4>
                 <p>{channel}</p>
                 <p>
